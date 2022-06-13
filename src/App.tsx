a14ba6908ab8526585cfc4f95c9bb443d18ba02a
gabriel <gabriel.gregorio.1@outlook.com>
2022-06-12 22:27:41 -0300

    Added updates and responsive version

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useState } from 'react';
+import { IconDark, IconLight, IconMenu } from './icons';
 import { SidebarBaseItemMenu } from './components/sidebarBaseItemMenu';
 import { SidebarBaseMenu } from './components/sidebarBaseMenu';
 import { GroupSuits } from './components/widgets/groupSuits';
@@ -39,15 +40,7 @@ const App = () => {
               type="button"
               onClick={() => setMenuIsOpen((prev) => !prev)}
               className="mr-2 lg:hidden border border-gray-300 dark:border-cyan-700 bg-gray-100 dark:bg-cyan-600 rounded-full p-2 text-cyan-600 dark:text-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth="2">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
+              <IconMenu />
             </button>
 
             <div className="mr-2">
@@ -71,35 +64,11 @@ const App = () => {
               />
 
               <div className="flex-1 p-1 flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                  />
-                </svg>
+                <IconLight />
               </div>
 
               <div className="flex-1 p-1 flex items-center justify-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                  />
-                </svg>
+                <IconDark />
               </div>
             </button>
           </div>
